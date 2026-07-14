462177590cd1a8b6d4b6430f9357bf19b5940b68
Akshit Kala <akshitkala72@gmail.com>
2026-07-14 13:01:37 +0530

    Time: 4 ms (51.56%), Space: 15 MB (12.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             int x=nums[i];
-            if(mp.count(target-x)){
+            if(mp.find(target-x)!=mp.end()){
                 return {mp[target-x],i};
             }else{
                 mp[x]=i;
